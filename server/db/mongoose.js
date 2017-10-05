3ffc2cf924c6b2ef3eb211a679f141597f2bb7e5
Keith Rodriguez <rodriguez.keith@gmail.com>
2017-10-04 20:38:59 -0500

    Fix connection to DB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// // need to set local environment variable or use npm dotenv
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost:27017/TodoApp');
+// var url = process.env.DATABASE_URL || 'mongodb://localhost:27017/TodoApp';
+// mongoose.connect(url);
+
+
 
 module.exports = {mongoose};
